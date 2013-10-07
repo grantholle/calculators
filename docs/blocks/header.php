70a5784dfa31ad0f2b33778b66be06c2a3a2829d
Besim Huskic <besim@40digits.com>
2013-10-07 12:07:59 -0500

    Changes

---
+++ docs/blocks/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head> 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1" />
+	<title>40D SASS Structure</title>
+	<!--[if lte IE 8]>
+    	<link rel="stylesheet" href="ie.css">
+	<![endif]-->
+	<!--[if gt IE 8]><!-->
+	    <link rel="stylesheet" href="style.css">
+	<!--<![endif]-->
+	<!--[if lt IE 9]>
+	<script src="/js/vendor/html5shiv.js"></script>
+	<![endif]-->
+	<script type="text/javascript" src="//use.typekit.net/tuw1waz.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+</head> 
+<body>
+
+<header>
+	<div class="row">
+		<h1>40D SASS Structure</h1>
+	</div>
+</header>
+
+<div class="site-wrap row">
+	<nav>
+		<ul>
+			<li><a href="#about">About</a></li>
+			<li>
+				<a href="#documentation">Documentation</a>
+				<ul>
+					<li><a href="#structure">Structure</a></li>
+					<li><a href="#mixins">Mixins</a></li>
+					<li><a href="#modules">Modules</a></li>
+					<li><a href="#partials">Partials</a></li>
+					<li><a href="#vendor">Vendor</a></li>
+				</ul>
+			</li>			
+		</ul>
+	</nav>
---
+++ docs/index.php
@@ -0,0 +1,59 @@
+<?php include 'blocks/header.php'; ?>
+
+<div id="home" class="custom">
+
+	<section id="structure">
+		<h2>Object Orient CSS</h2>
+		<p>The purpose of OOCSS is to encourage code reuse and, ultimately, faster and more efficient stylesheets that are easier to add and maintain. More importantly you are extending and reusing existing styles based on existing rule sets.</p>
+
+		<h3>There are 2 main principles of OOCSS:</h3>
+		<ol>
+			<li><strong>Separation of structure from skin</strong><br />ie: 3 different buttons utilizing same strucutre, but have different widths/heights</li>
+			<li><strong>Separation of containers and content</strong> <br />We're encouraged to give more forethought to what is common among different elements, then separate those common features into modules, or objects that can be reused anywhere. When we use OOCSS' class-based module building, we ensure that our styles are not dependent on any containing element.</li>
+		</ol>
+
+		<h3>Why should I code this way?</h3>
+		<ul>
+			<li>CSS is reusable</li>
+			<li>Stylesheets become smaller</li>
+			<li>Maintainable stylesheets (code is located in one block instead of many</li>
+			<li>Easier to improve one block of code instead of many</li>
+			<li>Easier to change parts of the site without breaking site</li>
+			<li>Enables you to change your site consistently</li>
+			<li>Faster website development</li>
+		</ul>
+
+		<h3>Dealing with smaller projects</h3>
+		<p>It certainly may seem like overkill for smaller projects, but in terms of our company growth and direction we're heading. Utilizing OOCSS concepts even in smaller projects is a great start, especially the practice going forward to larger projects.</p>
+
+
+		<h3>Guidelines for Implementation</h3>
+		<ul>
+			<li>Avoid descendent selectors (.sidebar h3)</li>
+			<li>Avoid IDs in styling hooks</li>
+			<li>Avoid attaching classes to elements in your stylesheets (div.header)</li>
+			<li>Avoid using !important</li>
+		</ul>
+
+		<h3>External Resources</h3>
+		<ul>
+			<li><a href="https://github.com/stubbornella/oocss/wiki">https://github.com/stubbornella/oocss/wiki</a></li>
+			<li><a href="http://www.vanseodesign.com/css/object-oriented-css/">http://www.vanseodesign.com/css/object-oriented-css//a></li>
+		</ul>
+
+		<h3>Object Oriented SASS</h3>
+		<ul>
+			<li><a href="https://portland2013.drupal.org/sites/default/files/slides/Sass_Silent_Classes_0.pdf">Utilizing %placeholder to extend your semantic CSS</a></li>
+			<li><a href="https://coderwall.com/p/7p7w2a">When to use SASS mixins, extends, variables, & %placeholders</a></li>
+			<li><a href="http://blog.adi.do/2012/09/object-oriented-media-queries-ooms-vs-media-oriented-objects-moo/">Object Oriented Media Queries</a></li>
+			<li><a href="http://blog.teamtreehouse.com/extending-placeholder-selectors-with-sass">Extending Placeholder Selectors with SASS</a></li>
+		</ul>
+	</section>
+
+	<section id="about">
+		about
+	</section>
+
+</div>
+
+<?php include 'blocks/footer.php'; ?>
